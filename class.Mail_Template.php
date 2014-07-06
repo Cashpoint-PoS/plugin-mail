87e890dce2903f3be50be5849f3e7ed41eca5e9a
Marco Schuster <marco+github@m-s-d.eu>
2014-07-06 19:13:13 +0200

    Mail_Template: more placeholders, attachment support

--- class.Mail_Template.php
+++ class.Mail_Template.php
@@ -33,6 +33,10 @@ class Mail_Template extends DBObj {
   );
   public static $links=array(
   );
+  
+  public static $one2many=array(
+  	"Mail_Template_Attachment"=>array("title"=>"Anhänge"),
+  );
   public function processProperty($key) {
     $ret=NULL;
     switch($key) {
@@ -42,15 +46,30 @@ class Mail_Template extends DBObj {
   public function toString() {
   	return $this->name." [".$this->subject."]";
   }
-  public function replacePlaceholders($customer,$str) {
+  public function replacePlaceholders($customer,$mailaddr,$str) {
+  	global $config;
   	$str=str_replace("%%SALUTATION%%",$customer->salutation,$str);
+  	$str=str_replace("%%OPTOUTURL%%",$config["paths"]["webroot"]."/optout.php?addr=".urlencode($mailaddr->addr),$str);
+  	$str=str_replace("%%TRACKURL%%",$config["paths"]["webroot"]."/track.png?addr=".urlencode($mailaddr->addr),$str);
+  	$str=str_replace("%%MAILENC%%",urlencode($mailaddr->addr),$str);
+  	$str=str_replace("%%MAILRAW%%",$mailaddr->addr,$str);
+  	
   	return $str;
   }
-  public function applyAndCreate($customer) {
+  public function applyAndCreate($customer,$mailaddr) {
   	$mail=Mail_Mail::fromScratch();
-  	$mail->subject=$this->replacePlaceholders($customer,$this->subject);
-  	$mail->content_html=$this->replacePlaceholders($customer,$this->content_html);
-  	$mail->content_text=$this->replacePlaceholders($customer,$this->content_text);
+  	$mail->subject=$this->replacePlaceholders($customer,$mailaddr,$this->subject);
+  	$mail->content_html=$this->replacePlaceholders($customer,$mailaddr,$this->content_html);
+  	$mail->content_text=$this->replacePlaceholders($customer,$mailaddr,$this->content_text);
+  	$mail->commit();
+  	$attachments=Mail_Template_Attachment::getByOwner($this);
+  	foreach($attachments as $att) {
+  		$at=Mail_Attachment::fromScratch();
+  		$at->mail_mails_id=$mail->id;
+  		$at->type=$att->type;
+  		$at->filerepo_files_id=$att->filerepo_files_id;
+  		$at->commit();
+  	}
   	return $mail;
   }
 }
